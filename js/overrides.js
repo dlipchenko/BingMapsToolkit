840261cf0e77488c7e6b939d3221a16725519f84
dlipchenko <dmytro.lipchenko@gmail.com>
2017-02-12 13:54:23 -0500

    Create overrides.js
    This file is meant to contain json objects that mimic the responses of the Bing Maps REST Locations API - the purpose is to utilize these if certain conditions are met instead of calling the Locations API for the results. This is good practice when the Locations API returns incorrect or unexpected data and a quick data fix is required instead of waiting for the Bing Maps support teams to fix it.

---
+++ js/overrides.js
@@ -0,0 +1,100 @@
+	  var overrideValues = { 
+		"NAIROBI": { 
+			totalResults: 2, 
+			resources:[
+				{
+					resultNumber: 1, 
+					name: "Nairobi, Kenya", 
+					entityType: "PopulatedPlace", 
+					coordinates: [
+						-1.273669958114624,
+						36.874900817871094
+					], 
+					confidence: "Override", 
+					AddressLine: "", 
+					Locality: "Nairobi", 
+					AdminDistrict: "Kenya", 
+					PostalCode: "", 
+					CountryRegion: "Kenya", 
+					AdminDistrict2: "", 
+					Neighborhood: "", 
+					RoutingCoordinates: [
+						-1.273669958114624,
+						36.874900817871094
+					]
+				}, 
+				{
+					resultNumber: 2, 
+					name: "Nairobi 2, Kenya", 
+					entityType: "PopulatedPlace", 
+					coordinates: [
+						-1.274517,
+						36.876446
+					], 
+					confidence: "Override", AddressLine: "", 
+					Locality: "Nairobi", 
+					AdminDistrict: "Kenya", 
+					PostalCode: "", 
+					CountryRegion: "Kenya", 
+					AdminDistrict2: "", 
+					Neighborhood: "", 
+					RoutingCoordinates: [
+						-1.274517,
+						36.876446
+					]
+				}
+			]
+		},
+		"WINNIPEG": {
+			totalResults: 1, 
+			resources:[
+				{
+					resultNumber: 1, 
+					name: "Winnipeg, MB", 
+					entityType: "PopulatedPlace", 
+					coordinates: [
+						49.91640090942383,
+						-97.20690155029297
+					], 
+					confidence: "Override", 
+					AddressLine: "", 
+					Locality: "Winnipeg", 
+					AdminDistrict: "MB", 
+					PostalCode: "", 
+					CountryRegion: "Canada", 
+					AdminDistrict2: "", 
+					Neighborhood: "", 
+					RoutingCoordinates: [
+						49.91640090942383,
+						-97.20690155029297
+					]
+				}
+			]
+		},
+		"WINNIPEG, MB": {
+			totalResults: 1, 
+			resources:[
+				{
+					resultNumber: 1, 
+					name: "Winnipeg, MB", 
+					entityType: "PopulatedPlace", 
+					coordinates: [
+						49.91640090942383,
+						-97.20690155029297
+					], 
+					confidence: "Override", 
+					AddressLine: "", 
+					Locality: "Winnipeg", 
+					AdminDistrict: "MB", 
+					PostalCode: "", 
+					CountryRegion: "Canada", 
+					AdminDistrict2: "", 
+					Neighborhood: "", 
+					RoutingCoordinates: [
+						49.91640090942383,
+						-97.20690155029297
+					]
+				}
+			]
+		}
+	  };
